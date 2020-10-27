caec1111d9cbd2685d6783aab58cb8d7f26fe3c5
timsmr <smrtim17@gmail.com>
2020-10-27 18:41:29 +0300

    vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,26 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    alph = "abcdefghijklmnopqrstuvwxyz"
+    j = 0
+
+    for i in range(len(plaintext)):
+        if plaintext[i].isalpha():
+            if alph.index(plaintext[i].lower()) + alph.index(keyword[j].lower()) >= len(alph):
+                g = alph.index(plaintext[i].lower()) + alph.index(keyword[j].lower()) - len(alph)
+            else:
+                g = alph.index(plaintext[i].lower()) + alph.index(keyword[j].lower())
+            if plaintext[i].isupper():
+                ciphertext += alph[g].upper()
+            else:
+                ciphertext += alph[g]
+            if j == len(keyword) - 1:
+                j = 0
+            else:
+                j += 1
+        else:
+            ciphertext += plaintext[i]
+
     return ciphertext
 
 
@@ -26,5 +45,23 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    alph = "abcdefghijklmnopqrstuvwxyz"
+    j = 0
+
+    for i in range(len(ciphertext)):
+        if ciphertext[i].isalpha():
+            if alph.index(ciphertext[i].lower()) - alph.index(keyword[j].lower()) < 0:
+                g = alph.index(ciphertext[i].lower()) - alph.index(keyword[j].lower()) + len(alph)
+            else:
+                g = alph.index(ciphertext[i].lower()) - alph.index(keyword[j].lower())
+            if ciphertext[i].isupper():
+                plaintext += alph[g].upper()
+            else:
+                plaintext += alph[g]
+            if j == len(keyword) - 1:
+                j = 0
+            else:
+                j += 1
+        else:
+            plaintext += ciphertext[i]
     return plaintext
